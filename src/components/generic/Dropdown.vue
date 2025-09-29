d014386121397322e4900d95dd42edda555747a5
anton bilichenko <re-z@ukr.net>
2025-09-29 20:28:14 +0300

    Add search block

---
+++ src/components/generic/Dropdown.vue
@@ -0,0 +1,42 @@
+<template>
+    <!-- TODO: add tests for the component -->
+    <div class="relative">
+        <Input
+            :size="props.size"
+            :placeholder="props.placeholder"
+            @focus="handleFocus(true)"
+            @blur="handleFocus(false)"
+        />
+        <ul
+            v-if="isDropdownVisible"
+            class="absolute w-full"
+        >
+            <li class="bg-gray-100 p-2">1</li>
+            <li class="bg-gray-100 p-2">2</li>
+            <li class="bg-gray-100 p-2">3</li>
+        </ul>
+    </div>
+</template>
+<script setup lang="ts">
+    import { INPUT_SIZE } from '@/components/generic/Input/Input.ts'
+    import Input from '@/components/generic/Input/Input.vue'
+    import { ref } from 'vue'
+    // props
+    const props = defineProps({
+        placeholder: {
+            type: String,
+            default: '',
+        },
+        size: {
+            //TODO: add strict validation from map INPUT_SIZE
+            type: String,
+            default: INPUT_SIZE.SM,
+        },
+    })
+    // focus
+    const isDropdownVisible = ref(false)
+
+    function handleFocus(isVisible: boolean) {
+        isDropdownVisible.value = isVisible
+    }
+</script>
---
+++ src/components/generic/Input/Input.vue
@@ -0,0 +1,36 @@
+<template>
+    <!-- TODO: add tests for the component -->
+    <div>
+        <input
+            class="rounded-1 rounded-sm bg-white outline-0"
+            :class="{
+                'px-4 py-2 text-xl': props.size === INPUT_SIZE.LG,
+                'bg-amber-100': props.size === INPUT_SIZE.MD,
+            }"
+            type="text"
+            :placeholder="props.placeholder"
+            @focus="emit('focus')"
+            @blur="emit('blur')"
+        />
+    </div>
+</template>
+<script setup lang="ts">
+    import { INPUT_SIZE } from './Input'
+
+    const emit = defineEmits<{
+        (e: 'focus'): void
+        (e: 'blur'): void
+    }>()
+    // props
+    const props = defineProps({
+        placeholder: {
+            type: String,
+            default: '',
+        },
+        size: {
+            //TODO: add strict validation from map INPUT_SIZE
+            type: String,
+            default: INPUT_SIZE.SM,
+        },
+    })
+</script>
