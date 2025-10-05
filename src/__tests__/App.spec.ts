11036081d3b7940845b8d30770b2415a86698b56
anton bilichenko <re-z@ukr.net>
2025-10-05 05:22:28 +0300

    Update test

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -2,6 +2,6 @@ import { describe, test, expect } from 'vitest'
 
 describe('Math test', () => {
     test('2 + 2 should equal 4', () => {
-        expect(2 + 2).toBe(8)
+        expect(2 + 2).toBe(4)
     })
 })
