1ad224d8cb5391f4efbedb6e3f48477131b78b31
Serhii Vorobei <s.vorobei.dev@gmail.com>
2025-10-05 14:22:36 +0300

    fixed router via router.push

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,14 +2,13 @@
     <footer class="bg-gray-500 px-10 py-15">
         <ul class="flex flex-col gap-3">
             <li>
-                <router-link
-                    to="tac"
+                <AppButton
                     class="inline-block px-1 text-lg text-white transition-all duration-300 hover:underline"
                     :class="{ 'bg-yellow-500': route.name === ROUTES.TAC_VIEW }"
+                    :type="BUTTON_TYPES.SECONDARY"
                     @click="navigateToPage(ROUTES.TAC_VIEW)"
-                >
-                    {{ $t('termsAndConditions') }}
-                </router-link>
+                    :text=" $t('termsAndConditions')"
+                />
             </li>
         </ul>
     </footer>
@@ -20,6 +19,8 @@
     import { ROUTES } from '@/router/routesMap'
 
     import { useRouter, useRoute } from 'vue-router'
+    import AppButton from '@/components/generic/AppButton/AppButton.vue'
+    import { BUTTON_TYPES } from '@/components/generic/AppButton/AppButton.ts'
     // router
     const router = useRouter()
     const route = useRoute()
