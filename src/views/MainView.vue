8a190e63fb5077a7f33a88dab4a4e1166e2345f8
Serhii Vorobei <102901051+sergey-vorobei@users.noreply.github.com>
2025-10-07 22:38:43 +0300

    Update src/views/MainView.vue
    Co-authored-by: Anton <re-z@ukr.net>

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -13,7 +13,7 @@
     import SeoLinksBlock from '@/components/SeoLinksBlock/SeoLinksBlock.vue'
     import SearchBlock from '@/components/SearchBlock/SearchBlock.vue'
     import SocialsBlock from '@/components/SocialsBlock/SocialsBlock.vue'
-    import AppHeader from '@/components/Header/Header.vue'
+   
     import AppFooter from '@/components/Footer/Footer.vue'
 </script>
 <style lang="scss"></style>
